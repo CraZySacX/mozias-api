68afa512a4dde2c36da6908c1bb44e9e7969f7de
Jason Ozias <jason.ozias@gmail.com>
2019-01-29 23:35:57 -0500

    telemetry into db

--- src/fairings/telemetry.rs
+++ src/fairings/telemetry.rs
@@ -13,12 +13,11 @@
 use crate::db;
 use crate::error::{MoziasApiErrKind, MoziasApiResult};
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
 use rocket::{Data, Request, Response};
 use std::time::Instant;
 use uuid::Uuid;
 
-const MOZIAS_UUID_HEADER: &str = "x-mozias-uuid";
+const MOZIAS_UUID_HEADER: &str = "x-request-id";
 
 #[derive(Clone, Default)]
 crate struct Telemetry {
@@ -28,10 +27,13 @@ crate struct Telemetry {
 
 impl Telemetry {
     fn request(&self, req: &mut Request<'_>, _: &Data) -> MoziasApiResult<()> {
-        let uuid = Uuid::new_v4();
-        let header = Header::new(MOZIAS_UUID_HEADER, uuid.to_hyphenated().to_string());
+        // let uuid = Uuid::new_v4();
+        let uuid_header = req
+            .headers()
+            .get_one(MOZIAS_UUID_HEADER)
+            .ok_or_else(|| MoziasApiErrKind::Header)?;
+        let uuid = Uuid::parse_str(uuid_header)?;
         let now = Instant::now();
-        req.add_header(header);
         let _ = req.local_cache(|| Self {
             start: Some(now),
             uuid: Some(uuid),
@@ -44,23 +46,14 @@ impl Telemetry {
             .headers()
             .get_one(MOZIAS_UUID_HEADER)
             .ok_or_else(|| MoziasApiErrKind::Header)?;
+        let uuid = Uuid::parse_str(uuid_header)?;
+        let uuid_str = uuid.to_hyphenated().to_string();
         let method = req.method().to_string();
-        let uri = req.uri().to_string();
-        let header_uuid = Uuid::parse_str(uuid_header)?;
+        let uri = req.uri().path();
         let telemetry = req.local_cache(|| Self {
             start: None,
             uuid: None,
         });
-
-        let uuid = if let Some(uuid) = telemetry.uuid.map(|u| u) {
-            if header_uuid == uuid {
-                uuid.to_hyphenated().to_string()
-            } else {
-                "unknown".to_string()
-            }
-        } else {
-            "unknown".to_string()
-        };
 
         let elapsed = if let Some(duration) = telemetry.start.map(|st| st.elapsed()) {
             duration.as_secs() * 1000 + u64::from(duration.subsec_millis())
@@ -68,7 +61,7 @@ impl Telemetry {
             0
         };
 
-        db::telemetry::insert_telemetry(&uuid, &method, &uri, elapsed)?;
+        db::telemetry::insert_telemetry(&uuid_str, &method, &uri, elapsed)?;
 
         Ok(())
     }
